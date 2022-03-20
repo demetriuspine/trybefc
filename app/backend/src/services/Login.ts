fa04a251af783b95c63075becc27518f70011e99
lucas <mr.pine97@gmail.com>
2022-03-20 15:21:17 -0300

    feat: cria o service LoginService

---
+++ app/backend/src/services/Login.ts
@@ -0,0 +1,28 @@
+import User from '../database/models/User';
+
+export default class LoginService {
+  static async getUser(email: string, password: string) {
+    const user = await User.findOne({
+      where: { email },
+    });
+    if (!user) return { message: 'Incorrect email or password' };
+
+    const match = password === user.password;
+
+    if (!match) return { message: 'Incorrect email or password' };
+
+    return {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+      email: user.email,
+    };
+  }
+
+  static async validateLogin(email: string) {
+    const user = await User.findOne({
+      where: { email },
+    });
+    return user;
+  }
+}
