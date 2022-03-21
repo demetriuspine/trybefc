71a96508b19e0875ab2d96430f2a68fcec78f82d
lucas <mr.pine97@gmail.com>
2022-03-21 11:37:03 -0300

    refact: usa numeros como statusCode

--- app/backend/src/controllers/Login.ts
+++ app/backend/src/controllers/Login.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import * as fileSystem from 'fs';
-import { StatusCodes } from 'http-status-codes';
 import { sign } from 'jsonwebtoken';
 import { RequestEmail } from '../interfaces/Interfaces';
 import LoginService from '../services/Login';
@@ -13,12 +12,12 @@ export default class LoginCOntroller {
       const { email, password } = req.body;
       const result = await LoginService.getUser(email, password);
 
-      if (result.message) return res.status(StatusCodes.UNAUTHORIZED).json(result.message);
+      if (result.message) return res.status(401).json(result.message);
 
       const token = sign({ email }, key);
-      return res.status(StatusCodes.OK).json({ user: result, token });
+      return res.status(200).json({ user: result, token });
     } catch (error) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+      return res.status(500).json(error);
     }
   }
 
@@ -27,9 +26,9 @@ export default class LoginCOntroller {
       const { email } = req;
       const data = email as string;
       const user = await LoginService.validateLogin(data);
-      if (user) res.status(StatusCodes.OK).json(user.role);
+      if (user) res.status(200).json(user.role);
     } catch (error) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+      return res.status(500).json(error);
     }
   }
 }
