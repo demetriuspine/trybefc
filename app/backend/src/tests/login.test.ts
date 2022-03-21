483f320d70d331363353d74432df4bb20cb0ad0a
lucas <mr.pine97@gmail.com>
2022-03-21 13:13:17 -0300

    tests: vers1

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,53 +1,163 @@
-
-  
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
+import User from '../database/models/User';
 
 import { Response } from 'superagent';
-import User from '../database/models/User';
-import {roleUser, successUserMock, unsuccessUserMock} from './mocks';
-
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Login route tests', () => {
-  
-  let chaiHttpResponse: Response;
+describe('Rota /login', () => {
 
-  before(async () => {
-    sinon
-      .stub(User, "findOne")
-      .resolves(successUserMock as User);
+  describe('Verifica se é possível fazer login com os dados corretos', () => {
+    let chaiHttpResponse: Response;
+
+    const mock = {
+      id: 1,
+      username: 'Admin',
+      email: 'admin@admin.com',
+      password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+      role: 'admin',
+    };
+
+    const loginData = {
+      email: 'admin@admin.com',
+      password: 'secret_admin',
+    };
+
+    const response = {
+      user: {
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+      },
+      token: '123.456.789',
+    };
+
+    before(async () => {
+      sinon.stub(User, 'findOne').resolves(mock as User);
+      chaiHttpResponse = await chai.request(app).post('/login').send(loginData);
+    });
+
+    after(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Recebe status 200, ao fazer login com dados corretos', async () => {
+      expect(chaiHttpResponse.status).to.be.eq(200);
+    });
+
+    it('A resposta recebe um user e um token', () => {
+      expect(chaiHttpResponse.body).to.have.keys(['token', 'user']);
+    });
+
+    it('A resposta tem user no formato adequado', () => {
+      expect(chaiHttpResponse.body.user).to.be.deep.equal(response.user);
+    });
   });
 
-  after(()=>{
-    (User.findOne as sinon.SinonStub).restore();
-  })
+  describe('Verifica que não é possível logar com um email invalido', () => {
+    let chaiHttpResponse: Response;
 
-  it('POST method /login success', async () => {
-    chaiHttpResponse = await chai
-       .request(app)
-       .post('/login')
-       .send(successUserMock)
-       
-    expect(chaiHttpResponse).to.have.status(200);
-    expect(chaiHttpResponse.body).to.be.a('object');
-    expect(chaiHttpResponse.body).to.include.all.keys('user','token');
+    const loginData = {
+      email: 'wronguser@admin.com',
+      password: '123456',
+    };
+
+    const message = 'Incorrect email or password';
+
+    before(async () => {
+      sinon.stub(User, 'findOne').resolves(null);
+      chaiHttpResponse = await chai.request(app).post('/login').send(loginData);
+    });
+
+    after(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Recebe um status 401 quando o email é invalido', () => {
+      expect(chaiHttpResponse.status).to.be.eq(401);
+    });
   });
 
-  it('POST method /login unsuccess', async () => {
-    chaiHttpResponse = await chai
-       .request(app)
-       .post('/login')
-       .send(unsuccessUserMock)
-       
-    expect(chaiHttpResponse).to.have.status(401);
-    expect(chaiHttpResponse.body).to.be.a('object');
-    expect(chaiHttpResponse.body).to.include.all.keys('message');
+  describe('Verifica que não é possível logar com uma senha inválida', () => {
+    let chaiHttpResponse: Response;
+
+    const loginData = {
+      email: 'admin@admin.com',
+      password: '123456',
+    };
+
+    const mock = {
+      id: 1,
+      username: 'Admin',
+      email: 'admin@admin.com',
+      password: '654321',
+      role: 'admin',
+    };
+
+    const message = 'Incorrect email or password';
+
+    before(async () => {
+      sinon.stub(User, 'findOne').resolves(mock as User);
+      chaiHttpResponse = await chai.request(app).post('/login').send(loginData);
+    });
+
+    after(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Recebe um status 401 quando a senha é invalida', () => {
+      expect(chaiHttpResponse.status).to.be.eq(401);
+    });
+  });
+
+  describe('Verifica que não é possível fazer login sem informar um email', () => {
+    let chaiHttpResponse: Response;
+
+    const loginData = {
+      password: '123456',
+    };
+
+    const message = 'All fields must be filled';
+
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).post('/login').send(loginData);
+    });
+
+    it('Recebe um status 401 quando o email não é informado', () => {
+      expect(chaiHttpResponse.status).to.be.eq(401);
+    });
+
+    it('Recebe a mensagem correta quando o email não é informado', () => {
+      expect(chaiHttpResponse.body).to.be.deep.equal({ message });
+    });
+  });
+
+  describe('Verifica que não é possível fazer login sem informar uma senha', () => {
+    let chaiHttpResponse: Response;
+
+    const loginData = {
+      email: 'admin@admin.com',
+    };
+
+    const message = 'All fields must be filled';
+
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).post('/login').send(loginData);
+    });
+
+    it('Recebe um status 401 quando a senha não é informada', () => {
+      expect(chaiHttpResponse.status).to.be.eq(401);
+    });
+
+    it('Recebe a mensagem correta quando a senha não é informada', () => {
+      expect(chaiHttpResponse.body).to.be.deep.equal({ message });
+    });
   });
 });
--- app/backend/src/tests/mocks.ts
+++ app/backend/src/tests/mocks.ts
@@ -1,10 +1,23 @@
 export const successUserMock = {
-  email: 'professor@chaos.com',
-  password: 'senha_super_segura_confia'
-}
+  id: 1,
+  username: 'Admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  role: 'admin',
+};
 
 export const unsuccessUserMock = {
   password: 'senha_super_segura_confia'
 }
 
 export const roleUser = 'professor_chaos';
+
+export const responseUser = {
+  user: successUserMock,
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiYWRtaW5AYWRtaW4uY29tIiwiaWF0IjoxNjQ3NjIxMjUyLCJleHAiOjE2NDgyMjYwNTJ9.6nOI48ao1XpKS1CSy3k3qQIqCCAYnqH-EZQsBa4QIjk"
+}
+
+export const loginData = {
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+};
