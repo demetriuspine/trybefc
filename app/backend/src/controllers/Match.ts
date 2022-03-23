ef74bd888c312e13a3b20a242d8c858baf8edc14
lucas <mr.pine97@gmail.com>
2022-03-23 09:37:58 -0300

    refact: altera o controller

--- app/backend/src/controllers/Match.ts
+++ app/backend/src/controllers/Match.ts
@@ -1,76 +1,54 @@
 import { Request, Response } from 'express';
-import Club from '../database/models/Club';
-import Match from '../database/models/Match';
+import { MatchMessageError } from '../types/MatchMessageError';
 import MatchService from '../services/Match';
 
 export default class MatchController {
-  static async getClubsByProgress(req: Request, res: Response) {
+  static async getAll(req: Request, res: Response) {
     try {
       const { inProgress } = req.query;
-
-      const status = (inProgress === 'true');
-
-      if (inProgress) {
-        const matches = await MatchService.getByStatus(status);
+      if (inProgress === undefined) {
+        const matches = await MatchService.getAll();
         return res.status(200).json(matches);
       }
-
-      const matches = await MatchService.getMatches();
-
+      const status = inProgress === 'true';
+      const matches = await MatchService.getAll(status);
       return res.status(200).json(matches);
     } catch (error) {
       return res.status(500).json(error);
     }
   }
 
-  static isMatchEqual(param1: Match, param2: Match) {
-    return param1 === param2;
-  }
-
-  static async create(req: Request, res: Response) {
+  static async createMatch(req: Request, res: Response) {
     try {
-      const { homeTeam, awayTeam } = req.body;
-      const isEqual = MatchController.isMatchEqual(homeTeam, awayTeam);
-
-      if (isEqual) {
-        return res.status(401)
-          .json({ message: 'It is not possible to create a match with two equal teams' });
-      }
-
-      const getHomeTeam = await Club.findByPk(homeTeam);
-      const getAwayTeam = await Club.findByPk(awayTeam);
-
-      if (!getHomeTeam || !getAwayTeam) {
-        return res.status(401).json({ message: 'Team not found' });
-      }
-
-      const createdMatch = await MatchService.create(req.body);
-      return res.status(201).json(createdMatch);
+      const dataMatch = req.body;
+      const match = await MatchService.createMatch(dataMatch);
+      const matchMessageError = match as MatchMessageError;
+      if (matchMessageError.message) return res.status(401).json(matchMessageError);
+      return res.status(201).json(match);
     } catch (error) {
-      return res.status(500).json('teste');
+      return res.status(500).json(error);
     }
   }
 
-  static async finishMatch(req: Request, res: Response) {
-    const { id } = req.params;
-    const { inProgress } = req.body;
-    const status = (inProgress === 'true');
-    const finishedMatch = await MatchService.finishMatch(id, status);
-    return res.status(200).json(finishedMatch);
+  static async updateStatus(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const result = await MatchService.updateStatus(id);
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 
-  static async updateMatch(req: Request, res: Response) {
-    const { id } = req.params;
-    const { homeTeamGoals, awayTeamGoals, inProgress } = req.body;
-
-    const status = (inProgress === 'true');
-
-    if (inProgress) {
-      const updatedMatch = await MatchService.updateMatch(id, homeTeamGoals, awayTeamGoals);
-      return res.status(200).json(updatedMatch);
+  static async updateMatchGoals(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { homeTeamGoals, awayTeamGoals } = req.body;
+      const result = await MatchService
+        .updateMatchGoals(id, homeTeamGoals, awayTeamGoals);
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).json(error);
     }
-
-    const finishedMatch = await MatchService.finishMatch(id, status);
-    return res.status(200).json(finishedMatch);
   }
 }
