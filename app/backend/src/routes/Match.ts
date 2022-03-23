250b344728af2452397ae2060fcd9a67c8ab55e8
lucas <mr.pine97@gmail.com>
2022-03-23 09:38:26 -0300

    refact: altera as rotas

--- app/backend/src/routes/Match.ts
+++ app/backend/src/routes/Match.ts
@@ -1,14 +1,16 @@
 import * as express from 'express';
 import validateToken from '../middlewares/validateToken';
 import MatchController from '../controllers/Match';
+import validateMatch from '../middlewares/validateMatch';
 
 const matchRouter = express.Router();
 
 matchRouter
-  .patch('/:id', MatchController.updateMatch)
-  .patch('/:id/finish', MatchController.finishMatch)
-  .get('/', MatchController.getClubsByProgress)
+  .patch('/:id', MatchController.updateMatchGoals)
+  .patch('/:id/finish', MatchController.updateStatus)
+  .get('/', MatchController.getAll)
   .use(validateToken)
-  .post('/', MatchController.create);
+  .use(validateMatch)
+  .post('/', MatchController.createMatch);
 
 export default matchRouter;
