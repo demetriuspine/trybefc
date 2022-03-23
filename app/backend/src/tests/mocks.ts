20969a9301ea33440bae879eb3f7d2627c546eb7
lucas <mr.pine97@gmail.com>
2022-03-23 17:27:58 -0300

    tentativa de rodar o avaliador novamente...

--- app/backend/src/tests/mocks.ts
+++ app/backend/src/tests/mocks.ts
@@ -14,8 +14,6 @@ export const unsuccesUserMock = {
   role: 'admin',
 };
 
-export const roleUser = 'professor_chaos';
-
 export const responseUser = {
   user: successUserMock,
   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiYWRtaW5AYWRtaW4uY29tIiwiaWF0IjoxNjQ3NjIxMjUyLCJleHAiOjE2NDgyMjYwNTJ9.6nOI48ao1XpKS1CSy3k3qQIqCCAYnqH-EZQsBa4QIjk"
